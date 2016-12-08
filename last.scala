2dd9bedf50e2e2d9826e586ed8574d354f1beee9
Connor Lewellyn <lewellync@gmail.com>
2016-12-08 16:49:36 -0500

    Finished problem 01

--- last.scala
+++ last.scala
@@ -1,5 +1,20 @@
-def last[A](ls: List[A]): A = ls match {
+def last1[A](ls: List[A]): A = {
+  ls.last
+}
+
+def last2[A](ls: List[A]): A = {
+  ls(ls.length-1)
+}
+
+// Nice and fancy method
+def last3[A](ls: List[A]): A = ls match {
   case h :: Nil   => h
-  case _ :: tail  => last(tail)
+  case _ :: tail  => last3(tail)
   case _          => throw new NoSuchElementException
 }
+
+val temp = List(1,2,3,4,5,6,7,4)
+
+println(last1(temp))
+println(last2(temp))
+println(last3(temp))
