e50945e4eff93ce9628b0114aa94bb02a27ea816
Connor Lewellyn <lewellync@gmail.com>
2016-12-27 11:53:36 -0500

    Finished 02

--- 02.scala
+++ 02.scala
@@ -7,7 +7,6 @@ def last[A](ls: List[A]): A =
     case _          => throw new NoSuchElementException
   }
 
-
 // The one that breaks with literally any checking
 def penultimate1[A](ls: List[A]): A =
   ls(ls.length-2)
@@ -17,7 +16,6 @@ def penultimate2[A](ls: List[A]): A =
   ls match {
     case h :: t :: Nil  => h
     case h :: tail      => penultimate2(tail)
-    case h :: Nil       => throw new IndexOutOfBoundsException
     case _              => throw new NoSuchElementException
   }
 
@@ -25,10 +23,10 @@ def penultimate2[A](ls: List[A]): A =
 def penultimate3[A](ls: List[A]): A =
   last(ls.dropRight(1))
 
-// Actual answer
+// Actual answer was close to penultimate2
 
-val temp = List(1)
+val temp = List(3,4,5,6,3,2,12,4)
 
-//println(penultimate1(temp))
+println(penultimate1(temp))
 println(penultimate2(temp))
 println(penultimate3(temp))
--- working_with_lists.scala
+++ working_with_lists.scala
@@ -6,3 +6,10 @@ def last[A](ls: List[A]): [A] =
     case _ :: tail  => last(tail)
     case _          => throw new NoSuchElementException
   }
+
+def penultimate[A](ls: List[A]): A =
+  ls match {
+    case h :: _ :: Nil  => h
+    case _ :: tail      => penultimate(tail)
+    case _              => throw new NoSuchElementException
+  }
